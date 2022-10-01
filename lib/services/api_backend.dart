9d332f23658337e97352cf42a38fa7dfeb5f3fa5
Paul <matovupaul2@gmail.com>
2022-10-01 21:56:42 +0200

    working on paul laptop

--- lib/services/api_backend.dart
+++ lib/services/api_backend.dart
@@ -1,13 +1,9 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-
 abstract class ApiService {
-  final String baseUrl = kDebugMode
-      ?
-      // "http://10.0.2.2:5068/"
-      "https://carappfinder1.azurewebsites.net/"
-      : "https://carappfinder1.azurewebsites.net/";
+  final String baseUrl =
+      // "http://10.0.2.2:5068/";
+      "https://carappfinder.azurewebsites.net/";
 
   final Map<String, String>? headers = {
     HttpHeaders.contentTypeHeader: 'application/json',
